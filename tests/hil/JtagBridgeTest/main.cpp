e58c770306b709587e42ad142ffd1ada2e143987
Andrew Zonenberg <azonenberg@drawersteak.com>
2017-03-31 00:39:46 -0700

    JtagBridgeTest: Began refactoring

--- tests/hil/JtagBridgeTest/main.cpp
+++ tests/hil/JtagBridgeTest/main.cpp
@@ -31,7 +31,7 @@
 	@file
 	@author Andrew D. Zonenberg
 	@brief NoC switch board ping test
-	
+
 	Try pinging a board through nocswitch
  */
 #include <stdio.h>
@@ -54,17 +54,21 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	int err_code = 0;//
+	int err_code = 0;
 	try
 	{
-		/*
-		//Connect to the server
 		string server;
 		int port = 0;
+
+		//Parse command-line arguments
 		for(int i=1; i<argc; i++)
 		{
 			string s(argv[i]);
-			
+
+			//Let the logger eat its args first
+			if(ParseLoggerArguments(i, argc, argv, console_verbosity))
+				continue;
+
 			if(s == "--port")
 				port = atoi(argv[++i]);
 			else if(s == "--server")
@@ -77,26 +81,30 @@ int main(int argc, char* argv[])
 				return 1;
 			}
 		}
+
+		//Set up logging
+		g_log_sinks.emplace(g_log_sinks.begin(), new ColoredSTDLogSink(console_verbosity));
+
+		//Connect to the server
 		if( (server == "") || (port == 0) )
 		{
-			throw JtagExceptionWrapper(
-				"No server or port name specified",
-				"",
-				JtagException::EXCEPTION_TYPE_GIGO);
-		}		
-		printf("Connecting to nocswitch server...\n");
-		NOCSwitchInterface iface;
+			LogError("No server or port name specified\n");
+			return 1;
+		}
+
+		LogNotice("Connecting to nocswitch server...\n");
+		/*NOCSwitchInterface iface;
 		iface.Connect(server, port);
-		
+
 		//Address lookup
 		printf("Looking up address of RPC pinger\n");
 		NameServer nameserver(&iface);
 		uint16_t pingaddr = nameserver.ForwardLookup("rpcping");
 		printf("Pinger is at %04x\n", pingaddr);
-		
+
 		//Needs to be deterministic for testing
 		srand(0);
-		
+
 		//Send a bunch of messages and make sure we get the same thing back
 		printf("Sending ping packets...\n");
 		double start = GetTime();
@@ -105,10 +113,10 @@ int main(int argc, char* argv[])
 		double minping = 999999;
 		double maxping = 0;
 		for(int i=0; i<npack; i++)
-		{	
+		{
 			if( (i % 100) == 0)
 				printf("Message %d\n", i);
-					
+
 			RPCMessage msg;
 			msg.from = 0xc000;
 			msg.to = 0x8002;
@@ -119,27 +127,27 @@ int main(int argc, char* argv[])
 			msg.data[2] = rand();
 			double tsend = GetTime();
 			iface.SendRPCMessage(msg);
-			
+
 			RPCMessage rxm;
 			if(!iface.RecvRPCMessageBlockingWithTimeout(rxm, 5))
 			{
 				printf("Timeout on message %d - expected response within 5 sec but nothing arrived\n", i);
 				printf("Sent:\n    %s\n\n", msg.Format().c_str());
-				
+
 				throw JtagExceptionWrapper(
 					"Message timeout",
 					"",
-					JtagException::EXCEPTION_TYPE_FIRMWARE);					
+					JtagException::EXCEPTION_TYPE_FIRMWARE);
 			}
-			
+
 			double trcv = GetTime();
 			double rtt = (trcv - tsend);
 			sumping += rtt;
 			if(minping > rtt)
 				minping = rtt;
 			if(maxping < rtt)
-				maxping = rtt;			
-			
+				maxping = rtt;
+
 			if( (rxm.from == pingaddr) &&
 				(rxm.data[0] == msg.data[0]) &&
 				(rxm.data[1] == msg.data[1]) &&
@@ -148,9 +156,9 @@ int main(int argc, char* argv[])
 			{
 				//printf("Message %d OK\n", i);
 			}
-			
+
 			else
-			{				
+			{
 				printf("Message %d FAIL\n", i);
 				printf("Sent:\n    %s\nReceived:\n    %s\n", msg.Format().c_str(), rxm.Format().c_str());
 				throw JtagExceptionWrapper(
@@ -159,14 +167,14 @@ int main(int argc, char* argv[])
 					JtagException::EXCEPTION_TYPE_FIRMWARE);
 			}
 		}
-		
+
 		int packets_sent = npack * 2;
 		int header_size = packets_sent * 32;
 		int payload_size = packets_sent * 96;
-		
+
 		//Print statistics
 		double end = GetTime();
-		double dt = end - start;	
+		double dt = end - start;
 		printf("Done, all OK\n");
 		printf("%d packets sent in %.2f ms (%.2f Kbps raw, %.2f Kbps after overhead)\n",
 			packets_sent,
@@ -179,7 +187,7 @@ int main(int argc, char* argv[])
 			maxping * 1000);
 		*/
 	}
-	
+
 	catch(const JtagException& ex)
 	{
 		//printf("%s\n", ex.GetDescription().c_str());
