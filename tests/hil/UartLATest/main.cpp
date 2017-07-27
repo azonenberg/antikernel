a1d9152b9198973a67114db663507a44024347a2
Andrew Zonenberg <azonenberg@drawersteak.com>
2017-07-26 20:19:12 -0700

    Initial skeleton of UART LA test code

---
+++ tests/hil/UartLATest/main.cpp
@@ -0,0 +1,213 @@
+/***********************************************************************************************************************
+*                                                                                                                      *
+* ANTIKERNEL v0.1                                                                                                      *
+*                                                                                                                      *
+* Copyright (c) 2012-2017 Andrew D. Zonenberg                                                                          *
+* All rights reserved.                                                                                                 *
+*                                                                                                                      *
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
+* following conditions are met:                                                                                        *
+*                                                                                                                      *
+*    * Redistributions of source code must retain the above copyright notice, this list of conditions, and the         *
+*      following disclaimer.                                                                                           *
+*                                                                                                                      *
+*    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the       *
+*      following disclaimer in the documentation and/or other materials provided with the distribution.                *
+*                                                                                                                      *
+*    * Neither the name of the author nor the names of any contributors may be used to endorse or promote products     *
+*      derived from this software without specific prior written permission.                                           *
+*                                                                                                                      *
+* THIS SOFTWARE IS PROVIDED BY THE AUTHORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   *
+* TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL *
+* THE AUTHORS BE HELD LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES        *
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR       *
+* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT *
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE       *
+* POSSIBILITY OF SUCH DAMAGE.                                                                                          *
+*                                                                                                                      *
+***********************************************************************************************************************/
+
+/**
+	@file
+	@author Andrew D. Zonenberg
+	@brief UART logic analyzer test
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <memory.h>
+#include <string>
+#include <list>
+#include <map>
+
+#include "../../../src/scopehal/RedTinLogicAnalyzer.h"
+
+#include "../../../src/jtaghal/jtaghal.h"
+#include "../../../src/nocbridge/nocbridge.h"
+
+#include "RPCv3Transceiver_types_enum.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int err_code = 0;
+	try
+	{
+		Severity console_verbosity = Severity::NOTICE;
+		string server;
+		int port = 0;
+		string tty;
+
+		//Parse command-line arguments
+		for(int i=1; i<argc; i++)
+		{
+			string s(argv[i]);
+
+			//Let the logger eat its args first
+			if(ParseLoggerArguments(i, argc, argv, console_verbosity))
+				continue;
+
+			if(s == "--port")
+				port = atoi(argv[++i]);
+			else if(s == "--server")
+				server = argv[++i];
+			else if(s == "--tty")
+				tty = argv[++i];
+			else
+			{
+				printf("Unrecognized command-line argument \"%s\", expected --server or --port\n", s.c_str());
+				return 1;
+			}
+		}
+
+		//Set up logging
+		g_log_sinks.emplace(g_log_sinks.begin(), new ColoredSTDLogSink(console_verbosity));
+
+		/*
+		//Connect to the server
+		if( (server == "") || (port == 0) )
+		{
+			LogError("No server or port name specified\n");
+			return 1;
+		}
+
+		LogNotice("Connecting to nocswitch server...\n");
+		NOCSwitchInterface iface;
+		iface.Connect(server, port);
+
+		//Allocate an address for us
+		uint16_t ouraddr;
+		if(!iface.AllocateClientAddress(ouraddr))
+		{
+			LogError("Couldn't allocate an address\n");
+			return 1;
+		}
+		LogNotice("Got address %04x\n", ouraddr);
+		*/
+
+		//Create the LA
+		RedTinLogicAnalyzer la(tty, 115200);
+
+		/*
+		//Address lookup
+		//printf("Looking up address of RPC pinger\n");
+		//NameServer nameserver(&iface);
+		//uint16_t pingaddr = nameserver.ForwardLookup("rpcping");
+		uint16_t pingaddr = 0xfeed;
+		LogNotice("Pinger is at %04x\n", pingaddr);
+
+		//Needs to be deterministic for testing
+		srand(0);
+
+		//Send a bunch of messages and make sure we get the same thing back
+		LogNotice("Sending ping packets...\n");
+		double start = GetTime();
+		int npack = 1000;
+		double sumping = 0;
+		double minping = 999999;
+		double maxping = 0;
+		for(int i=0; i<npack; i++)
+		{
+			if( (i % 100) == 0)
+				LogNotice("Message %d\n", i);
+
+			RPCMessage msg;
+			msg.from = ouraddr;
+			msg.to = pingaddr;
+			msg.type = RPC_TYPE_INTERRUPT;
+			msg.callnum = rand() & 0xff;
+			msg.data[0] = rand() & 0x001fffff;
+			msg.data[1] = rand();
+			msg.data[2] = rand();
+			double tsend = GetTime();
+			iface.SendRPCMessage(msg);
+			//LogDebug("Sending: %s\n", msg.Format().c_str());
+
+			RPCMessage rxm;
+			if(!iface.RecvRPCMessageBlockingWithTimeout(rxm, 5))
+			{
+				LogError("Timeout on message %d - expected response within 5 sec but nothing arrived\n", i);
+				LogVerbose("Sent:\n    %s\n\n", msg.Format().c_str());
+
+				throw JtagExceptionWrapper(
+					"Message timeout",
+					"");
+			}
+			//LogDebug("Got: %s\n", rxm.Format().c_str());
+
+			double trcv = GetTime();
+			double rtt = (trcv - tsend);
+			sumping += rtt;
+			if(minping > rtt)
+				minping = rtt;
+			if(maxping < rtt)
+				maxping = rtt;
+
+			if( (rxm.from == pingaddr) &&
+				(rxm.data[0] == msg.data[0]) &&
+				(rxm.data[1] == msg.data[1]) &&
+				(rxm.data[2] == msg.data[2])
+			)
+			{
+				//LogDebug("Message %d OK\n", i);
+			}
+			else
+			{
+				LogError("Message %d FAIL\n", i);
+				LogError("Sent:\n    %s\nReceived:\n    %s\n", msg.Format().c_str(), rxm.Format().c_str());
+				throw JtagExceptionWrapper(
+					"Invalid message came back",
+					"");
+			}
+		}
+
+		int packets_sent = npack * 2;
+		int header_size = packets_sent * 96;
+		int payload_size = packets_sent * 128;
+
+		//Print statistics
+		double end = GetTime();
+		double dt = end - start;
+		LogNotice("Done, all OK\n");
+		LogNotice("%d packets sent in %.2f ms (%.2f Kbps raw, %.2f Kbps after overhead)\n",
+			packets_sent,
+			dt * 1000,
+			(payload_size + header_size) / (1000 * dt),
+			(payload_size) / (1000 * dt));
+		LogNotice("RTT latency: %.2f ms min / %.2f ms avg / %.2f max\n",
+			minping * 1000,
+			(sumping / npack) * 1000,
+			maxping * 1000);
+		*/
+	}
+
+	catch(const JtagException& ex)
+	{
+		LogError("%s\n", ex.GetDescription().c_str());
+		err_code = 1;
+	}
+
+	//Done
+	return err_code;
+}
---
+++ tests/hil/UartLATestBitstream/UartLATestBitstream.v
@@ -0,0 +1,147 @@
+/***********************************************************************************************************************
+*                                                                                                                      *
+* ANTIKERNEL v0.1                                                                                                      *
+*                                                                                                                      *
+* Copyright (c) 2012-2017 Andrew D. Zonenberg                                                                          *
+* All rights reserved.                                                                                                 *
+*                                                                                                                      *
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
+* following conditions are met:                                                                                        *
+*                                                                                                                      *
+*    * Redistributions of source code must retain the above copyright notice, this list of conditions, and the         *
+*      following disclaimer.                                                                                           *
+*                                                                                                                      *
+*    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the       *
+*      following disclaimer in the documentation and/or other materials provided with the distribution.                *
+*                                                                                                                      *
+*    * Neither the name of the author nor the names of any contributors may be used to endorse or promote products     *
+*      derived from this software without specific prior written permission.                                           *
+*                                                                                                                      *
+* THIS SOFTWARE IS PROVIDED BY THE AUTHORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   *
+* TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL *
+* THE AUTHORS BE HELD LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES        *
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR       *
+* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT *
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE       *
+* POSSIBILITY OF SUCH DAMAGE.                                                                                          *
+*                                                                                                                      *
+***********************************************************************************************************************/
+
+module UartLATestBitstream(
+	input wire clk,
+    output wire[3:0] led,
+    inout wire[7:0] pmod_e
+    );
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // The Zynq CPU (not actually used for anything for now, just to make DRC shut up)
+
+    // Only define this if we're targeting a Zynq part
+	`ifdef XILINX_ZYNQ7
+
+	XilinxZynq7CPU cpu(
+		.cpu_jtag_tdo(),
+		.cpu_jtag_tdi(1'b0),
+		.cpu_jtag_tms(1'b0),
+		.cpu_jtag_tck(1'b0),
+
+		//Don't use any of these signals
+		.__nowarn_528_cpu_clk(),
+		.__nowarn_528_cpu_por_n(),
+		.__nowarn_528_cpu_srst_n()
+	);
+
+	`endif
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Buffer the main system clock
+
+	wire clk_bufg;
+	ClockBuffer #(
+		.TYPE("GLOBAL"),
+		.CE("NO")
+	) sysclk_clkbuf (
+		.clkin(clk),
+		.clkout(clk_bufg),
+		.ce(1'b1)
+	);
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // The debug bridge
+
+    wire		rpc_tx_en;
+    wire[31:0]	rpc_tx_data;
+    wire		rpc_tx_ready;
+
+    wire		rpc_rx_en;
+    wire[31:0]	rpc_rx_data;
+    wire		rpc_rx_ready;
+
+    JtagDebugBridge #(
+		.NOC_WIDTH(32)
+    ) bridge(
+		.clk(clk_bufg),
+
+		//RPC interface
+		.rpc_tx_en(rpc_tx_en),
+		.rpc_tx_data(rpc_tx_data),
+		.rpc_tx_ready(rpc_tx_ready),
+
+		.rpc_rx_en(rpc_rx_en),
+		.rpc_rx_data(rpc_rx_data),
+		.rpc_rx_ready(rpc_rx_ready),
+
+		//Debug indicators
+		.led(led)
+	);
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Loopback core
+
+    RPCv3EchoNode #(
+		.NOC_WIDTH(32),
+		.NOC_ADDR(16'hfeed)
+	) echo (
+		.clk(clk_bufg),
+
+		.rpc_tx_en(rpc_rx_en),
+		.rpc_tx_data(rpc_rx_data),
+		.rpc_tx_ready(rpc_rx_ready),
+
+		.rpc_rx_en(rpc_tx_en),
+		.rpc_rx_data(rpc_tx_data),
+		.rpc_rx_ready(rpc_tx_ready)
+	);
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // The LA
+
+    assign pmod_e[7:4] = 4'h0;
+    assign pmod_e[1:0] = 1'h0;
+    assign pmod_e[3] = 1'bz;
+
+    RedTinUartWrapper #(
+		.WIDTH(96),
+		.DEPTH(512),
+		.UART_CLKDIV(16'd1085),	//115200 @ 125 MHz
+		.SYMBOL_ROM(
+			{
+				16384'h0,
+				"DEBUGROM",
+				{ "rpc_rx_en", 8'd1, 8'h0 },
+				{ "rpc_rx_ready", 8'd1, 8'h0},
+				{ "rpc_rx_data", 8'd32, 8'h0},
+				{ "rpc_tx_en", 8'd1, 8'h0 },
+				{ "rpc_tx_ready", 8'd1, 8'h0},
+				{ "rpc_tx_data", 8'd32, 8'h0}
+			}
+		)
+	) analyzer (
+		.clk(clk_bufg),
+		.capture_clk(clk_bufg),
+		.din(),
+		.uart_rx(pmod_e[3]),
+		.uart_tx(pmod_e[2])
+	);
+
+endmodule
